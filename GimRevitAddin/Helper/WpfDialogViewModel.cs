62860226cba94e07c04ca7304258bdd2fa16b437
rascer <erichraschle@gmail.com>
2019-04-25 00:07:08 +0200

    Dialog with UserControl implemented

---
+++ GimRevitAddin/Helper/WpfDialogViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Gim.Domain.Helpers.Event;
+using System.Windows.Input;
+
+namespace Gim.Revit.Addin.Helper
+{
+    public class WpfDialogViewModel : ANotifyPropertyChangedModel
+    {
+        private readonly IDialogContentViewModel contentModel;
+        public WpfDialogViewModel(IDialogContentViewModel dialogContent)
+        {
+            contentModel = dialogContent;
+        }
+
+        public object CommandParameter
+        {
+            get { return contentModel.CommandParameter; }
+            set
+            {
+                contentModel.CommandParameter = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ICommand okayCommand;
+        public ICommand OkayCommand
+        {
+            get { return okayCommand; }
+            set
+            {
+                okayCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ICommand cancelCommand;
+        public ICommand CancelCommand
+        {
+            get { return cancelCommand; }
+            set
+            {
+                cancelCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
